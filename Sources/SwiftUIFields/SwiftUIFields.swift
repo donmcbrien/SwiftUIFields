c1492a7fe346268270d4268d7f7f8e4f651fbaff
donmcbrien <don.mcbrien@icloud.com>
2021-05-20 12:30:22 +0100

    Initial Commit

---
+++ Sources/SwiftUIFields/SwiftUIFields.swift
@@ -0,0 +1,3 @@
+struct SwiftUIFields {
+    var text = "Hello, World!"
+}
---
+++ Tests/SwiftUIFieldsTests/SwiftUIFieldsTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import SwiftUIFields
+
+    final class SwiftUIFieldsTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(SwiftUIFields().text, "Hello, World!")
+        }
+    }
